18b253064d8d67211e26da00fe0dfcba25b1489c
NaosAntares <phyzheye01@gmail.com>
2020-12-28 20:36:51 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./styles/App.scss";
+import Header from "./styles/components/header";
+import Banner from "./styles/components/banner";
 
 function App() {
+  
+  useEffect(() => {
+    let vh = window.innerHeight * 0.1;
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+  }, []);
+
   return (
     <div className='App'>
-      <h2>Hello this is your starter files </h2>
+      <Header />
+      <Banner />
     </div>
   );
-}
+};
 
 export default App;
